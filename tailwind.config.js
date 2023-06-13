6bde0da1aaa143e3599468e0c11c551cbb20e4f2
harshadladva <harshadladva123@gmail.com>
2023-06-13 23:45:38 +0530

    feat: update the breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  screens: {
-    xs: "280px",
-    sm: "480px",
-    md: "768px",
-    lg: "976px",
-    xl: "1440px",
-  },
   theme: {
+    screens: {
+      xs: "280px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
